ca7e99e08c80cb0f91dd038029ed192de1558b93
ilkay murat ertekin <iertekinx@gmail.com>
2021-12-05 23:53:28 +0300

    Add user controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,86 @@
+const User = require("../models/User");
+
+
+exports.getAllUser = async (req, res) => {
+  try {
+    const users = await User.find({}).select({ password: 0 });
+    res.status(200).json({
+      status: "Success",
+      message: "All users",
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
+
+exports.getUserDetail = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.status(200).json({
+        status: "Success",
+        message: "User Detail",
+        data: {
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email,
+          phone: user.phone,
+          role: user.role,
+          department: user.department,
+        },
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.deleteOne();
+      res.status(200).json({
+        status: "Success",
+        message: "User deleted",
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    console.log(req.body)
+    
+      user.first_name= req.body.first_name,
+      user.last_name= req.body.last_name,
+      user.email= req.body.email,
+      user.phone= req.body.phone,
+      user.password= req.body.password,
+      user.save()
+    res.status(200).json({
+      status: "Success",
+      message: "User Updated",
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: "Failed",
+      message: err.message,
+    });
+  }
+};
